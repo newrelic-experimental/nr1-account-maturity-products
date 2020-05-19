e13190c33501024afef3d9433278745cb29549c7
swinters <swinters@newrelic.com>
2020-05-19 16:37:19 +0100

    mobile application and process score clean up

--- src/modules/mobile/MobileApplication.js
+++ src/modules/mobile/MobileApplication.js
@@ -1,30 +1,14 @@
-const semver = require('semver');
-
 class MobileApplication {
-  constructor(entity, account) {
+  constructor(entity) {
     // entity, entitySearch result model see apm-gql.js
     this.id = entity.applicationId;
     this.name = entity.name;
     this.guid = entity.guid;
     this.accountId = entity.accountId;
 
-    this.appLaunchCount = entity.appLaunchCount
-
     this.reporting = entity.reporting;
 
     this.healthStatus = entity.alertSeverity;
-    this.alertConditions = [];
-    this.labels = [];
-    if (entity.tags) {
-      this.labels = entity.tags
-        .map(tag => tag.key)
-        .filter(
-          key =>
-            ['account', 'accountId', 'language', 'trustedAccountId'].indexOf(
-              key
-            ) === -1
-        );
-    }
   }
 
   isAlerting() {
@@ -33,15 +17,6 @@ class MobileApplication {
         this.healthStatus.indexOf('NOT_CONFIGURED') === -1 ||
         this.healthStatus.indexOf('') === -1
       ) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  hasLabels() {
-    if (this.reporting) {
-      if ((this.labels || []).length > 0) {
         return true;
       }
     }
