8f9376cccb8cd1734282445b7b6466f8bcba85b4
Benedicto Tan <tanben@users.noreply.github.com>
2020-06-16 10:11:47 -0400

    enable overview  table loading interstitial UI

--- src/modules/overview/OverviewTable.js
+++ src/modules/overview/OverviewTable.js
@@ -6,11 +6,16 @@ import matchSorter from 'match-sorter';
 
 export function OverviewTable(props) {
   const { data, keys } = props;
-  const summaryHeader = props.summaryHeader || OVERVIEW_ACCOUNT_HEADER;
-  const columnHeaders = getColumnHeaders(keys, [...summaryHeader]);
+  const columnHeaders = [...OVERVIEW_ACCOUNT_HEADER, ProductColumns];
+  const isLoading = keys && keys.length < ProductColumns.columns.length;
   return (
     <div>
-      <ReactTable data={data} columns={columnHeaders} filterable />
+      <ReactTable
+        data={data}
+        columns={columnHeaders}
+        filterable
+        loading={isLoading}
+      />
       {CreateCSVLink(columnHeaders, data)}
     </div>
   );
@@ -35,73 +40,56 @@ export const OVERVIEW_ACCOUNT_HEADER = [
   }
 ];
 
-function getColumnHeaders(productKeys, summaryHeader) {
-  const headers = summaryHeader;
-
-  const productHeader = {
-    Header: 'Account Maturity Scores',
-    columns: []
-  };
-
-  const productHeaderDef = {
-    APM_SCORE: {
+export const ProductColumns = {
+  Header: 'Account Maturity Scores',
+  columns: [
+    {
       Header: 'APM',
       accessor: 'APM_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     },
-    BROWSER_SCORE: {
+    {
       Header: 'Browser',
       accessor: 'BROWSER_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     },
-    SYNTHETICS_SCORE: {
+    {
       Header: 'Synthetics',
       accessor: 'SYNTHETICS_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     },
-    INFRASTRUCTURE_SCORE: {
+    {
       Header: 'Infrastructure',
       accessor: 'INFRASTRUCTURE_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     },
-    INSIGHTS_SCORE: {
+    {
       Header: 'Insights',
       accessor: 'INSIGHTS_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     },
-    LOG_SCORE: {
+    {
       Header: 'Log',
       accessor: 'LOG_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     },
-    PROGRAMMABILITY_SCORE: {
+    {
       Header: 'Programmability',
       accessor: 'PROGRAMMABILITY_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     },
-    MOBILE_SCORE: {
+    {
       Header: 'Mobile',
       accessor: 'MOBILE_SCORE',
       Cell: row => cellRenderer(row),
       filterable: false
     }
-  };
-
-  productKeys.forEach(prodKey => {
-    if (!productHeaderDef[prodKey]) {
-      throw new Error(`OverviewPanel : Unknown key ${prodKey}.`);
-    }
-    productHeader.columns.push(productHeaderDef[prodKey]);
-  });
-
-  headers.push(productHeader);
-
-  return headers;
-}
+  ]
+};
